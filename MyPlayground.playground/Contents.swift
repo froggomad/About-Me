3d7c42459afda786b3e1f28d019814666ae028a5
Kenny Dubroff <kennydubroff@hazystudios.com>
2019-12-03 10:34:42 -0800

    Basic Struct

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,3 +1,11 @@
 var name = "Kenny Dubroff"
 var specialty = "Full Stack iOS Development"
 var hobbies = ["Video Games","Learning Programming Languages and Methodologies"]
+
+struct Me {
+    var name: String
+    var specialty: String
+    var hobbies: [String]
+}
+
+let kennyDubroff = Me(name: name, specialty: specialty, hobbies: hobbies)
